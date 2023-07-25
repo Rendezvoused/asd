408010f4222ad45f4bf8b80ca1e5c7f154a84a5b
Rendezvoused <89423721+Rendezvoused@users.noreply.github.com>
2023-07-24 22:14:00 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -13,8 +13,8 @@ new Vue({
           name: "Mekanın Sahibi",
           artist: "Norm Ender",
           cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/1.jpg",
-          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/1.mp3",
-          url: "https://www.youtube.com/watch?v=z3wAjJXbYzA",
+          source: "https://raw.githubusercontent.com/Rendezvoused/asd/mp3/1.mp3",
+          url: "https://www.youtube.com/watch?v=TbKMSv2VBZM",
           favorited: false
         },
         {
